3a7f97a369d2d2b0f90e6741d7ad9979be7112c1
Varshini Neelamegan <varshini2010.nm@gmail.com>
2026-04-22 07:26:20 +0530

    Add files via upload

---
+++ largest_value.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a, b;
+    cout<<"enter two values:";
+    cin >> a >> b;
+    if(a > b)
+        cout <<"the largest value is "<<a;
+    else
+        cout <<"the largest value is "<<b;
+    return 0;
+}
