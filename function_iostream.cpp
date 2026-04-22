0017b70c7028afbe91bf6646d4359e25016ce145
Varshini Neelamegan <varshini2010.nm@gmail.com>
2026-04-22 07:36:09 +0530

    Add files via upload

---
+++ function_iostream.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string name;
+	cout<<"ENTER YOU NAME MADAM:";
+	cin>>name;
+	if(name=="varshini")
+{
+	cout<<"YOU ARE WELCOME MAM"<<endl;
+	clog<<"we are happy";
+	}
+	else
+	{
+	cerr<<"YOU ARE NOT ALLOWED!"<<endl;
+	clog<<"invalid";
+    }
+	return 0;
+}
